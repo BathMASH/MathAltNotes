343719361bc443cf2e932fd3af6c662242f742ce
Emma Cliffe <cspehj@mapc-lapsup18.campus.bath.ac.uk>
2018-07-20 16:52:58 +0100

    Information on how to produce tests now correct

--- pipes/template/README.md
+++ pipes/template/README.md
@@ -1,3 +1,3 @@
-To create a new test place the LaTeX in input.tex and then run quickMake on this. Assuming this works without problems copy the following file list to the outputs folder:
+To create a new test place the LaTeX in input.tex. Run quickReady and say no to everything. Then make standard, clear, large and word. Assuming this works without problems copy the following file list to the outputs folder:
 
 cp choices.tex input.tex input.fls input.parens macros.tex input-co input-cont.tex input-cont-clean.tex input-cont-clean-lp.tex input-word.css input-word.html input-word.html.tmp input-word.html.tmp2 ../units/TESTNAME/output/
---
+++ scripts/quickMake/quickpipes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+usage() {
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "THIS IS A TEST SCRIPT. It takes no arguments. It will run all" 
+echo "available pipe tests in the directory pipes. That could take a "
+echo "substantial amount of time to complete. Tea and biscuits time!"
+echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+exit
+}
+
+for i in `find . -wholename "./pipes/*-text"`; do
+echo $i
+./getMyLaTeX $i/input &>/dev/null
+read input < .lasttimestamp
+#echo $input
+./quickTest $input $i
+rm -r $input
+sleep 2
+done
