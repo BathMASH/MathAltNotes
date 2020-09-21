54440ff84a665b80d22885396086737c7372479b
Emma Cliffe <cspehj@bath.ac.uk>
2020-09-21 19:56:15 +0100

    Fixing tidy so that it doesn't remove whitespace that is actually important

--- scripts/TeX4htPostprocessing/postprocess.sh
+++ scripts/TeX4htPostprocessing/postprocess.sh
@@ -7,7 +7,7 @@ cd $1
 for i in $(ls *.html); do
 cp $i $i.tmp
 java -classpath $jarloc/tex4ht.jar xtpipes -i $xtpipeloc -s $home/groupmn.4xt -o $i $i.tmp
-tidy -m -q -xml -w --preserve-entities yes -ashtml -access 3 -i $i
+tidy -m -q -w --preserve-entities yes -ashtml -access -i $i
 #sed -i '/^$/d' $i
 sed -i '/^[[:space:]]*$/d' $i
 #rm $i.tmp
