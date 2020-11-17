31c6f861d0870f2273eba029a32d2bdc4f47e6c1
Emma Cliffe <cspehj@bath.ac.uk>
2020-11-17 21:04:53 +0000

    Adding quick delivery for Moodle format to quickMake and suppressing the warnings from tidy since they were repetitive and not informative

--- scripts/TeX4htPostprocessing/postprocessFB.sh
+++ scripts/TeX4htPostprocessing/postprocessFB.sh
@@ -9,7 +9,7 @@ cp $i $i.tmp
 java -classpath $jarloc/tex4ht.jar xtpipes -i $xtpipeloc -s $home/groupmn.4xt -o $i.tmp2 $i.tmp
 #sed -i '/^$/d' $i.tmp2
 java -classpath $jarloc/tex4ht.jar xtpipes -i $xtpipeloc -s $home/FB.4xt -o $i $i.tmp2 
-tidy -m -q -xml -w --preserve-entities yes -ashtml -access 3 -i $i
+tidy -m -q -xml -w --preserve-entities yes --show-warnings no -ashtml -access 3 -i $i
 sed -i '/^[[:space:]]*$/d' $i
 #rm $i.tmp
 done 
